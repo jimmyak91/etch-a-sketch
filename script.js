50ff0195ea20a93f5b94f6eb401c8b80d1fd69be
James Keech <james.a.keech@gmail.com>
2021-09-13 09:26:18 +1000

    updated codebase to support multiple drawing modes.

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
+const DEFAULT_COLOUR = "#333333";
+const DEFAULT_SIZE = 16;
+const DEFAULT_MODE = 'colour';
+
+let currentColour = DEFAULT_COLOUR;
+let currentMode = DEFAULT_MODE;
+let currentSize = DEFAULT_SIZE;
+
+function setCurrentColour(newColour) {
+    currentColour = newColour;
+}
+
+function setCurrentMode(newMode) {
+    currentMode = newMode;
+}
+
+function setCurrentSize(newSize) {
+    currentSize = newSize;
+}
+
+
+
+
 var gridAxisSize = 16;
 var container = document.querySelector('.container');
 var gridSize = gridAxisSize * gridAxisSize;
 function createGrid (size) {
-    squareSize = 700 / gridAxisSize;
+    squareSize = 600 / gridAxisSize;
     console.log(squareSize);
     console.log(gridSize);
     for (let x = 0; x < gridSize; x++) {
@@ -17,11 +40,11 @@ function createGrid (size) {
 
 }
 
-createGrid(gridSize);
+// createGrid(gridSize);
 
 const setColour = document.querySelector('#colour-picker');
 const colour = document.querySelector('input');
-var choosenColour;
+var choosenColour = '#333333';
 
 colour.addEventListener('input', function( event ) {
     setColour.style.backgroundColor = this.value;
@@ -52,4 +75,59 @@ clearButton.addEventListener("click", function( event ) {
 
 
 //////////////////////////////
+// MOUSE OVER STYLES
 
+// clearButton.addEventListener('mouseenter', function( event ) {
+//     clearButton.setAttribute('style', 'zoom: 120%;');
+// });
+
+// clearButton.addEventListener('mouseleave', function( event ) {
+//     clearButton.setAttribute('style', 'zoom: 100%;');
+// });
+
+// setColour.addEventListener('mouseenter', function( event ) {
+//     setColour.setAttribute('style', 'zoom: 120%;');
+// });
+
+// setColour.addEventListener('mouseleave', function( event ) {
+//     setColour.setAttribute('style', 'zoom: 100%;');
+
+// });
+
+function changeColor(e) {
+    if (currentMode === 'rainbow') {
+      const randomR = Math.floor(Math.random() * 256)
+      const randomG = Math.floor(Math.random() * 256)
+      const randomB = Math.floor(Math.random() * 256)
+      e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+    } else if (currentMode === 'colour') {
+      e.target.style.backgroundColor = currentColor
+    } else if (currentMode === 'eraser') {
+      e.target.style.backgroundColor = '#fefefe'
+    }
+  }
+
+
+function activateButton(newMode) {
+    if (currentMode === 'rainbow') {
+      rainbowBtn.classList.remove('active')
+    } else if (currentMode === 'color') {
+      colorBtn.classList.remove('active')
+    } else if (currentMode === 'eraser') {
+      eraserBtn.classList.remove('active')
+    }
+  
+    if (newMode === 'rainbow') {
+      rainbowBtn.classList.add('active')
+    } else if (newMode === 'color') {
+      colorBtn.classList.add('active')
+    } else if (newMode === 'eraser') {
+      eraserBtn.classList.add('active')
+    }
+  }
+  
+  window.onload = () => {
+    createGrid(DEFAULT_SIZE)
+    console.log("On WIndow load")
+    // activateButton(DEFAULT_MODE)
+  }
